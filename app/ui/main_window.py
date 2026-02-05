b05dfa5fcf4cad153e038baf3b79278f606aedcc
chubchik <eduardcastello007@gmail.com>
2026-02-05 13:11:01 +0200

    v2.2.9

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1745,13 +1745,13 @@ class MainWindow(QMainWindow):
                         continue
                     delta_eirp = delta_eirp_for(dist_km, diff_loss, env_loss)
                     if delta_eirp >= 5.0:
-                        pixels[i, j] = (34, 197, 94, 140)
+                        pixels[i, j] = (0, 200, 83, 200)
                         tile_has = True
                     elif delta_eirp >= 0.0:
-                        pixels[i, j] = (245, 158, 11, 140)
+                        pixels[i, j] = (255, 208, 0, 200)
                         tile_has = True
                     elif delta_eirp >= -5.0:
-                        pixels[i, j] = (239, 68, 68, 140)
+                        pixels[i, j] = (255, 23, 68, 200)
                         tile_has = True
             if not tile_has:
                 return None

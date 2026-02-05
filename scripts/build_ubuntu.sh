aac1a4ddd620d5b31c1d2bfe46389fe3f7dde71a
chubchik <eduardcastello007@gmail.com>
2026-02-05 13:54:57 +0200

    workflow bugs fixed

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
 python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 python -m pip install pyinstaller
 
 pyinstaller -y pyinstaller.spec
